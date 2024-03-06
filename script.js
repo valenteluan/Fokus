600ca2e9a923d9afc7310bc31c3aab7ad9009081
Luan Valente <luanbvalente@hotmail.com>
2024-03-06 13:13:57 -0300

    Separando funções e métodos

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900; 
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco');
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo');
 })
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+}
