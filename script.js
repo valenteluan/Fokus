3f43b6e86cb29c99fe83788ef8044d78dc5900bc
Luan Valente <luanbvalente@hotmail.com>
2024-03-06 20:53:51 -0300

    Adicionando e Removendo classes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button');
 
 const botaoIniciar = document.querySelector('.app__card-primary-button');
 const displayTempo = document.querySelector('#timer');
@@ -14,19 +15,26 @@ const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
+
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
